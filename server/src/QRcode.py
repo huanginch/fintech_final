cd776ce36ebe1e7eb6373dfe8c2af20f3472a13d
felix <felix0412a@icloud.com>
2021-06-18 23:15:45 +0800

    QR函式

--- server/src/QRcode.py
+++ server/src/QRcode.py
@@ -1,17 +1,15 @@
 import qrcode
-
+from random import randint
 #qr = qrcode.make('hello world')
 #qr.save('myQR.png')
 
-qr = qrcode.QRCode(
-    version = 1,
-    #error_correction = qrcode.constants.ERROR_CORRECT
-    box_size = 14,
-    border = 3
-)
 
-data = 'abcdefghijklmnopqrstuvwxyz'
-qr.add_data(data)
-qr.make(fit = True)
-img = qr.make_image(fill = 'black', back_color = 'white')
-img.save('testing.png')
+def QRcode():
+    qr = qrcode.QRCode(version = 1,box_size = 14,border = 3)
+    a = randint(10000000, 99999999)
+    data = str(a)
+    qr.add_data(data)
+    qr.make(fit = True)
+    img = qr.make_image(fill = 'black', back_color = 'white')
+    img.save('testing.png')
+
