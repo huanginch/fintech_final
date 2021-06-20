1daa9cef7e57a9f106746a3377b9b35599353e7e
Laxiflora <elton2000113@gmail.com>
2021-06-20 20:12:55 +0800

    123

--- server/src/test_use.py
+++ server/src/test_use.py
@@ -16,8 +16,9 @@ ticket_type = "搖滾區"
 
 
 json = {'qrcode':qrcode , 'event':event , 'ticket_type': ticket_type , 'username':username}
-py_firebase.init()
-
+#py_firebase.init()
+py_firebase.generate_data()
+"""
 py_firebase.setData(json,"addTicket")
 json2 = {'qrcode':"test2" , 'event':"test2" , 'ticket_type': "test2" , 'username':username}
 py_firebase.setData(json2,"addTicket")
@@ -27,3 +28,4 @@ test2 = {'username':'gahua', 'event': "柯基跳舞秀"}
 print(py_firebase.getData(test,"checkTicket"))
 print(py_firebase.getData(test2,"checkTicket"))
 
+"""
