f821541ee23da1ab333ba3d72baad1c427577e0c
Laxiflora <elton2000113@gmail.com>
2021-06-21 15:47:23 +0800

    資料庫更新

--- server/src/py_firebase.py
+++ server/src/py_firebase.py
@@ -5,6 +5,38 @@ import json
 cred_obj = None
 default_app = None
 
+
+"""
+使用前請確認有先在app.py跑過一次py_firebase.init()
+
+方法:
+getData(data,method):主要用於從資料庫取得資料，data為索引(格式為json/字典)，method為要做的事情(格式為字串)
+    method = "login" : 用於登入，data = {'username' , 'password'}
+                                回傳:布林值
+
+    method = "checkTicket" : 用於驗證某活動的票卷是否已經買過，data = {'username' , 'event'}
+                            回傳: 布林值
+
+    method = "getTicketInfo" : 用於取得某使用者的票卷購買詳細資訊，data = {'username' , ('event'可給可不給) }
+                                回傳: {'QRcode', 'orderID', 'qrcode', 'ticket_type'}
+                                    {'event': {'QRcode', 'orderID', 'qrcode', 'ticket_type'} ,...} if no event sent
+
+
+setData(data,method):主要用於送給資料庫資料，data為索引(格式為json/字典)，method為要做的事情(格式為字串)
+    method = "addTicket" : 用於新增票卷(家華)，data = {username , oriderID, event, ticket_type}
+    method = "generateQR" : 以orderID產生QRcode，data = {username , orderID , QRcode}
+
+
+"""
+
+
+
+
+
+
+
+
+
 def init():
     cred_obj = firebase_admin.credentials.Certificate('firebase_key/fintech-61df3-firebase-adminsdk-4vr8z-629365b6cf.json')
     default_app = firebase_admin.initialize_app(cred_obj, {
@@ -40,18 +72,38 @@ def getData(data,method):
             return False
         else:                             #not any ticket has been bought yet
             return False
+
+    elif method =="getTicketInfo":
+        username = data['username']
+        ref = db.reference("/userAccount/"+username+"/tickets")
+        ticketList = ref.get()
+        if 'event' in data:         # has event name
+            if data['event'] in ticketList:
+                return ticketList[data['event']]
+        else:
+            return ticketList
+        
     else:
         return "undefined method"
     
 
 
 def setData(data,method):
-    if method == "addTicket":       #data = {username , qrcode, event, ticket_type}
+    if method == "addTicket":       #data = {username , oriderID, event, ticket_type}
         ref = db.reference("/userAccount/"+data['username']+"/tickets/"+data['event'])
         data.pop('username')
         data.pop('event')
         ref.update(data)
-
+    
+    if method == "generateQR":      #data = {username , orderID , QRcode}
+        ref = db.reference("/userAccount/"+data['username']+"/tickets/")
+        ticketList = ref.get()
+        for (tickey,ticval) in ticketList.items():
+            if ticval['orderID'] == data['orderID']:
+                temp = data.pop('QRcode')
+                ref = ref.child(tickey)
+                ref.update({"QRcode":temp})
+                break
 
 
 def generate_data():
@@ -75,18 +127,3 @@ def generate_data():
 
 #generate_data()
 
-"""
-cred_obj = firebase_admin.credentials.Certificate('firebase_key/fintech-61df3-firebase-adminsdk-4vr8z-629365b6cf.json')
-default_app = firebase_admin.initialize_app(cred_obj, {
-	'databaseURL':'https://fintech-61df3-default-rtdb.firebaseio.com/'
-	})
-ref = db.reference("/userAccount")
-#ref.push({'test':'test'})
-#print( )
-print( ref.get().values())
-#print("gahua" in ref.get().values())
-
-for i in ref.get().values():
-    print("gahua" in i)
-    print("corgi87" == i['gahua'])
-"""
